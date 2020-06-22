6970483c448ba1c32ebd6064e25d76b7fcbba661
hera <a.heraimenka@gmail.com>
2020-06-22 13:27:31 -0700

    Add columns

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import './App.scss';
 function App() {
     return (
         <div className="App">
-            <h1>Heading h1</h1>
-            <h2>Heading h2</h2>
-            <h3>Heading h3</h3>
-            <h4>Heading h4</h4>
-            <h5>Heading h5</h5>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-        
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6">
+                        col md 6
+                    </div>
+                    <div className="col-md-6">
+                        col md 6
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
