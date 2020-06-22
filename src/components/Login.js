172d25e756c891715cdf006a8e25170716bb859f
Vince Williams <williamsvm20@gmail.com>
2020-06-22 15:45:19 -0400

    added the jsx for Register and Login components

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,6 +3,20 @@ import React from 'react'
 
 export default function Login(props) {
     return (
-        null
+        <div>
+            <div>
+                <img src='http://placekitten.com/200/300' alt='Secret Family Recipe Logo' />
+            </div>
+            <form onSubmit=''>
+                <h2>Login</h2>
+                <label>Email:
+                    <input name='email' type='text' />
+                </label>
+                <label>
+                    <input name='password' type='text' />
+                </label>
+                <button>Login</button>
+            </form>
+        </div>
     )
 }
--- src/components/Register.js
+++ src/components/Register.js
@@ -2,6 +2,20 @@ import React from 'react'
 
 export default function Register(props) {
     return (
-        null
+        <div>
+            <div>
+                <img src='http://placekitten.com/200/300' alt='Secret Family Recipe Logo' />
+            </div>
+            <form onSubmit=''>
+                <h2>Register</h2>
+                <label>Email:
+                    <input name='email' type='text' />
+                </label>
+                <label>
+                    <input name='password' type='text' />
+                </label>
+                <button>Register</button>
+            </form>
+        </div>
     )
 }
