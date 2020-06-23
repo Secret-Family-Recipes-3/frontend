a8705a4b209a227da6a58f157d7e19d45ef4aade
hera <a.heraimenka@gmail.com>
2020-06-22 17:14:12 -0700

    Add styles for top panel

---
+++ src/components/TopPanel/TopPanel.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function TopPanel () {
+    return (
+        <div className='topPanel'>
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-lg-12'>
+                        <ul className="topPanel__nav">
+                            <li>
+                                <a href='/#'>Log In</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
