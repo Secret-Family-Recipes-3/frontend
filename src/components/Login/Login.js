a52d3ed3674ec1983af05498cf55ec34452029f2
Vince Williams <williamsvm20@gmail.com>
2020-06-23 13:39:26 -0400

    login page is working

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Login.scss';
+import * as yup from 'yup'
+import {loginSchema} from '../../schema/formSchema'
 
 
 export default function Login(props) {
-    const { onChange, submitLogin } = props
+    const initialErrors = {
+        username: '',
+        password: ''
+    }
+
+    const [errors, setErrors] = useState(initialErrors)
+    const [submitButton, setSubmitButton] = useState(true)
+    const [formValue, setFormValue] = useState() 
+
+
+
+
+    const onChange = event => {
+        const name = event.target.name
+        const fieldValue = event.target.value
+        
+        // console.log(`${name}: ${value}`)
+
+        yup.reach(loginSchema, name)
+            .validate(formValue)
+            .then(valid => {
+                setErrors({...errors, [name]: ''})
+            })
+            .catch(err => {
+                setErrors({...errors, [name]: err.errors[0]})
+            })
+
+
+        setFormValue({...formValue, [name]: fieldValue})
+    }
+
+    useEffect(() => {
+        loginSchema.isValid(formValue)
+        .then(valid => {
+            setSubmitButton(!valid)
+        })
+    },)
+
+    const submitLogin = event => {
+        event.preventDefault()
+        
+        console.log(`username: ${formValue.username}, password: ${formValue.password}`)
+    }
+
 
     return (
         <div className='Login'>
@@ -19,11 +64,13 @@ export default function Login(props) {
 
                                 <label htmlFor='username'>Username:</label>
                                 <input name='username' type='text' id='username' onChange={onChange} />
+                                {errors.username.length > 0 ? (<p className="error">{errors.username}</p>) : null}
 
                                 <label htmlFor='password'>Password:</label>
                                 <input name='password' type='password' id='password' onChange={onChange} />
+                                {errors.password.length > 0 ? (<p className="error">{errors.password}</p>) : null}
 
-                                <button className='button button-green'>Log In</button>
+                                <button disabled={submitButton} className='button button-green'>Log In</button>
                             </form>
                         </div>
                     </div>
