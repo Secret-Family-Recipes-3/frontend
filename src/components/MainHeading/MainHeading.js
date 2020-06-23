668156c117443504b5676956f1a704864e4d0cd0
hera <a.heraimenka@gmail.com>
2020-06-22 18:48:51 -0700

    Add MainHeading component

---
+++ src/components/MainHeading/MainHeading.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './MainHeading.scss';
+
+export default function MainHeading () {
+    return (
+        <div className='MainHeading'>
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-lg-12'>
+                        <h2>All Recipes</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
