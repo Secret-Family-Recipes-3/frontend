97e498da4262cfa08cc6731c83282d52da493714
Vince Williams <williamsvm20@gmail.com>
2020-06-24 13:08:16 -0400

    updated with recipe card generator '

--- src/components/RecipeList/RecipeList.js
+++ src/components/RecipeList/RecipeList.js
@@ -1,62 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './RecipeList.scss';
 import Header from '../Header/Header';
+import axios from 'axios'
+import { Link } from 'react-router-dom'
+
 
 export default function RecipeList () {
+
+    const initialrecipes = []
+
+    const [ recipes, setRecipes ] = useState(initialrecipes)
+
+    useEffect(() => {
+        axios.get('https://secretfamilyrecipe3.herokuapp.com/') 
+            .then(res => {
+                const newRecipes = res.data
+                setRecipes({...recipes, newRecipes})
+            })
+            .catch(err => console.log(err))
+    }, [])
+
+
     return (
         <>
             <Header />
             <div className='RecipeList'>
                 <div className='container'>
                     <div className='row'>
-                        <div className='col-md-6 col-lg-4 RecipeList__recipe'>
-                            <a href='/#'>
-                                <div className='RecipeList__image-wrapper'>
-                                    <img src="https://images.pexels.com/photos/1630588/pexels-photo-1630588.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt='recipe' />
+                        {recipes.map(recipe => {
+                            return (
+                                <div className='col-md-6 col-lg-4 RecipeList__recipe'>
+                                    <Link to={`/recipes/${recipe.id}`}>
+                                        <div className='RecipeList__image-wrapper'>
+                                            <img src={recipe.img} alt={recipe.title} />
+                                        </div>
+                                    </Link>
+                                    <h5>{recipe.title}</h5>
                                 </div>
-                            </a>
-                            <h5>Title</h5>
-                        </div>
-                        <div className='col-md-6 col-lg-4 RecipeList__recipe'>
-                            <a href='/#'>
-                                <div className='RecipeList__image-wrapper'>
-                                    <img src="https://images.pexels.com/photos/2090903/pexels-photo-2090903.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt='recipe' />
-                                </div>
-                            </a>
-                            <h5>Title</h5>
-                        </div>
-                        <div className='col-md-6 col-lg-4 RecipeList__recipe'>
-                            <a href='/#'>
-                                <div className='RecipeList__image-wrapper'>
-                                    <img src="https://images.pexels.com/photos/4198283/pexels-photo-4198283.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt='recipe' />
-                                </div>
-                            </a>
-                            <h5>Title</h5>
-                        </div>
-                        <div className='col-md-6 col-lg-4 RecipeList__recipe'>
-                            <a href='/#'>
-                                <div className='RecipeList__image-wrapper'>
-                                    <img src="https://images.pexels.com/photos/4020664/pexels-photo-4020664.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt='recipe' />
-                                </div>
-                            </a>
-                            <h5>Title</h5>
-                        </div>
-                        <div className='col-md-6 col-lg-4 RecipeList__recipe'>
-                            <a href='/#'>
-                                <div className='RecipeList__image-wrapper'>
-                                    <img src="https://images.pexels.com/photos/1211887/pexels-photo-1211887.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt='recipe' />
-                                </div>
-                            </a>
-                            <h5>Title</h5>
-                        </div>
-                        
-                        <div className='col-md-6 col-lg-4 RecipeList__recipe'>
-                            <a href='/#'>
-                                <div className='RecipeList__image-wrapper'>
-                                </div>
-                            </a>
-                            <h5>A Recipe with no image</h5>
-                        </div>
+                            )
+                        }) }
                         
                     </div>
                 </div>
