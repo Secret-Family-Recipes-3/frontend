d866c898a092fb1cae809cfff993521e0728b727
Vince Williams <williamsvm20@gmail.com>
2020-06-25 15:57:56 -0400

    cleaned up the code - removed experimental code from earlier.

--- src/components/RecipeTile/RecipeTile.js
+++ src/components/RecipeTile/RecipeTile.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 
 export default function RecipeTile (props) {
     const { recipe } = props;
+    // const defaultImg = 'https://images.unsplash.com/photo-1546250683-afa23f23dde1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80'
 
     return (
         <div key={recipe.id} className='col-md-6 col-lg-4 RecipeList__recipe'>
